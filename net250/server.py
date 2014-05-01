60ba3e0233d339520d28f816c261f9b5f0b6f195
ravane_a <ravane_a@epitech.eu>
2014-05-01 11:23:14 +0000

    add net

--- net250/server.py
+++ net250/server.py
@@ -4,16 +4,17 @@ from socket import *
 import thread
 import sys
 
-FLAG = "sin"
+FLAG = "IMTHEFLAG"
 HOST = '0.0.0.0'
 PORT = int(sys.argv[1]) if (len(sys.argv) > 1) else 1337
 
 def handler(client, addr):
+    client.send("guess what is it you may need others day binaries -> \"8<7;$%? :\"\n")
     while 1:
         data = client.recv(1024)
         if not data: break
         if FLAG == data.rstrip():
-            client.send("You win !\n")
+            client.send("You win ! flag is \"C00k1e4Y0u\"")
             print addr[0] + " - flagged - " + repr(data.rstrip())
         else:
             client.send("You loose ! Try again ...\n")
